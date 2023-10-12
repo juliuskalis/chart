32ccb0807f8534c5f41b280e915b3e5530882b05
juliuskalis <julius.kalis@gmail.com>
2023-10-12 22:42:46 +0200

    move groupsDataFeature.selector.ts to folder

--- src/app/bussiness-domain/store/selectors/rawChartData/groups.selector.ts
+++ src/app/bussiness-domain/store/selectors/rawChartData/groups.selector.ts
@@ -1,6 +1,6 @@
 import {createSelector} from "@ngrx/store";
 import {selectUsersData} from "../usersDataFeature.selector";
-import {selectGroupsData} from "../groupsDataFeature.selector";
+import {selectGroupsData} from "./groupsDataFeature.selector";
 import {GroupWithPeopleModel, UserResponseModel, StoreDataWithGroupIdModel} from "../../../models/userResponse.model";
 import {GroupsResponseModel} from "../../../models/groupsResponse.model";
 import {getUserInGroupRule} from "../../../rules/groupSelection/getUserInGroup.rule";
